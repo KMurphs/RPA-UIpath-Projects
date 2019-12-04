b2cc5b1c6126bd491c30c7667f49d03272bb5030
stephane kibonge <stephane.kibonge@etion.co.za>
2019-12-04 09:35:31 +0200

    Write Bat File to Execute Robot every 10min

--- Projects/SortGmail/runTask.bat
+++ Projects/SortGmail/runTask.bat
@@ -1,13 +1,31 @@
+
 @echo off
+echo Starting GMail Sorting...
+REM Use 'setlocal' so that we can use an environment variable without it leaking out to the caller.
+setlocal
+
+REM Initialize the counter to zero
+set counter=0
+:AGAIN
+
+REM Increment the counter
+set /A counter+=1
+echo Processing loop %counter%...
+
+REM echo date /t
+REM echo time /t
+echo Current Timestamp: %DATE% %TIME%
 
 
+
+REM Check the counter to see if we've done enough iterations--bail out if we have
+REM if '%counter%'=='11' goto END
+
+
+REM Saving Source Directory and Robot Directory
 SET startDir=%~dp0
 SET robotDir="C:\Users\stephane.kibonge.PARSEC\AppData\Local\UiPath\app-19.10.1"
 
-
-
-REM echo %startDir%
-REM echo %robotDir%
 
 echo Going Into Robot Directory
 cd %robotDir%
@@ -17,3 +35,19 @@ UiRobot.exe execute --file "%startDir%\Main.xaml"
 
 echo Returning To Original Directory
 cd %startDir%
+
+
+echo Next Execution will happen in 10minutes...
+
+
+REM Wait for one second so as not to peg the CPU
+timeout /T 600 /NOBREAK>nul
+
+
+goto AGAIN
+
+
+:END
+
+REM End the local processing and go back to the main environment
+endlocal
